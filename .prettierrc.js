8a15aa49996540293a4f33fa55f0f2efa7c01d0a
NexusFeng <1622920569@qq.com>
2022-04-07 15:12:19 +0800

    add: 集成husky&eslint&prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "printWidth": 80,
+  "semi": false,
+  "singleQuote": true,
+  "jsxSingleQuote": false,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": true,
+  "arrowParens": "avoid",
+  "insertPragma": false,
+  "tabWidth": 2,
+  "useTabs": false,
+  "HTMLWhitespaceSensitivity": "ignore"
+}
--- src/components/table/util.js
+++ src/components/table/util.js
@@ -1,23 +1,21 @@
-
-export const getCell = function(event) {
-  let cell = event.target;
+export const getCell = function (event) {
+  let cell = event.target
 
   while (cell && cell.tagName.toUpperCase() !== 'HTML') {
     if (cell.tagName.toUpperCase() === 'TD') {
-      return cell;
+      return cell
     }
-    cell = cell.parentNode;
+    cell = cell.parentNode
   }
 
-  return null;
-};
+  return null
+}
 
 export function parseWidth(width) {
   if (width) {
-    width = parseInt(width, 10);
-  } else{
+    width = parseInt(width, 10)
+  } else {
     width = 0
   }
-  return width;
+  return width
 }
-
